8024dd5a3ff0cf8e172bf86361be9b5b69a27ed7
Musaib Bin Bashir <musaibbashir02@gmail.com>
2026-01-12 20:42:58 +0530

    fixed server side key rendering

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_ADMIN_PASSWORD: process.env.NEXT_PUBLIC_ADMIN_PASSWORD,
+  },
 }
 
 export default nextConfig
